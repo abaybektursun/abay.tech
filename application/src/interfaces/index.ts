c9b250372769bef751c2b00bf9768fae582e0077
Abay Bektursun <abaybektursun@gmail.com>
2024-11-13 07:41:03 +0000

    bsic working

---
+++ application/src/interfaces/index.ts
@@ -0,0 +1,25 @@
+// src/interfaces/index.ts
+export type User = {
+  name: string;
+  picture: string;
+  sub: string;
+  email?: string;
+};
+
+export type Comment = {
+  id: string;
+  created_at: number;
+  url: string;
+  text: string;
+  user: User;
+};
+
+export type Post = {
+  slug?: string;
+  title?: string;
+  author?: string;
+  date?: Date;
+  content?: string;
+  excerpt?: string;
+  [key: string]: any;
+};
