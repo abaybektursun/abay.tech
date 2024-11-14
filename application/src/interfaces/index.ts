fd1eecbfd16c11c8fb0e4b0b8cf58af57aa8dc89
Abay Bektursun <abaybektursun@gmail.com>
2024-11-14 09:52:49 +0000

    #1 and removed auth + old comments

--- application/src/interfaces/index.ts
+++ application/src/interfaces/index.ts
@@ -1,19 +1,4 @@
 // src/interfaces/index.ts
-export type User = {
-  name: string;
-  picture: string;
-  sub: string;
-  email?: string;
-};
-
-export type Comment = {
-  id: string;
-  created_at: number;
-  url: string;
-  text: string;
-  user: User;
-};
-
 export type Post = {
   slug?: string;
   title?: string;
