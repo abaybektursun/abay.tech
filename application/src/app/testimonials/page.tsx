c72573288947de4861b25d706e9e7adae5f2acd0
abaybektursun <abaybektursun@gmail.com>
2024-11-17 16:34:54 +0700

    testimonials focus

--- application/src/app/testimonials/page.tsx
+++ application/src/app/testimonials/page.tsx
@@ -1,5 +1,8 @@
+// src/app/testimonials/page.tsx
+'use client';
 import { Reviews } from "@/components/reviews";
 import { reviews } from "@/data/testimonials";
+
 
 export default function ReviewsPage() {
   return <Reviews reviews={reviews} />;
