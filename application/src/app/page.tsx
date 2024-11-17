2c2ea1bbb7e56b218c7f5d367effef3b01e009f2
abaybektursun <abaybektursun@gmail.com>
2024-11-17 18:51:04 +0700

    #17

--- application/src/app/page.tsx
+++ application/src/app/page.tsx
@@ -1,16 +1,17 @@
 // src/app/page.tsx
 'use client';
 
-import Image from "next/image";
 import Container from "@/components/container";
 import { motion } from 'framer-motion';
 import { variants, transition } from '@/lib/animations';
 import RotatingText from '@/components/RotatingText';
 import { ExternalLinks } from "@/components/external";
-import ProgressiveImage from "@/components/progressive"
+import PixelatedImage from "@/components/progressive"
 
 
 export default function HomePage() {
+  const lowResUrl = `/_next/image?url=${encodeURIComponent('/DSC07673.jpg')}&w=40&q=25`;
+
   return (
     <Container>
       <div className="flex flex-col md:flex-row md:items-center md:gap-12">
@@ -22,13 +23,14 @@ export default function HomePage() {
           transition={{ ...transition, delay: 0.2 }}
         >
           <div className="relative aspect-[3/2] w-full rounded-2xl overflow-hidden shadow-[0_8px_30px_rgb(0,0,0,0.06)]">
-        <ProgressiveImage
-        src="/DSC07673.jpg"
-        alt="Description"
-        width={2048}
-        height={1365}
-        className="rounded-lg" // optional
-      />
+          <PixelatedImage
+            src="/DSC07673.jpg"
+            lowResSrc={`/_next/image?url=${encodeURIComponent('/DSC07673.jpg')}&w=64&q=75`}
+            alt="Description"
+            width={2048}
+            height={1365}
+            className="rounded-lg"
+          />
           </div>
         </motion.div>
 
