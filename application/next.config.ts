db5f6097ad967918d062798a4f18a878b846daf4
abaybektursun <abaybektursun@gmail.com>
2025-01-21 17:10:21 -0600

    working Batch norm post!

--- application/next.config.ts
+++ application/next.config.ts
@@ -10,6 +10,20 @@ const nextConfig: NextConfig = {
 
     return config;
   },
+  async redirects() {
+    return [
+      {
+        source: '/blog/2018/07/01/why-does-batch-normalization-work',
+        destination: '/posts/why-does-batch',
+        permanent: true,
+      },
+      {
+        source: '/blog/2023/07/08/unseen-opportunities-mining-upwork-with-gpt',
+        destination: '/posts/upwork',
+        permanent: true,
+      },
+    ]
+  }
 };
 
 export default nextConfig;
